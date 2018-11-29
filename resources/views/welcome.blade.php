6145de41badee8a8a8d7b4df1d25229d4270bb72
laurabombonatinho <laurabombonato04@gmail.com>
2018-11-29 17:06:35 -0200

    segundo commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,6 @@
 
 @section('content')
 
-<div class="page-header">
-    <div class="container-fluid">
-        <h2 class="h5 no-margin-bottom">Sugestões</h2>
-    </div>
-</div>
     <p>oi</p>
     <p>Thaisinhaaaaaaaaaaaaaaaaaaaaa</p>
     <p>Thaisinhaaaaaaaaaaaaaaaaaaaaa</p>
